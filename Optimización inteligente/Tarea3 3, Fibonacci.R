10bfce56dad0f54422d79885d43b80bcd1a14b84
poface64 <angelgb064@gmail.com>
2024-10-01 11:16:24 -0600

    Tarea 3 de optimización

---
+++ Optimización inteligente/Tarea3 3, Fibonacci.R
@@ -0,0 +1,81 @@
+rm(list = ls())
+
+# Mecanismo generador de fibonacci
+FIBO = function(n){
+  # Crear una lista inicial con los valores F_0=1 y F_1 = 1
+  FV = c(1,1)
+  if(n>=2){
+    # Hacer el ciclo que la construya poco a poco
+    for(i in 3:(n+1) ){
+      # Desde el elemnto 3 hasta el elemento n, suma los 2 anteriores
+      FV[i] = FV[i-1] + FV[i-2]
+    }
+    # Nombrar los datos en orden
+    names(FV) = 0:(n)
+    FV[paste0(n)]
+  }else{
+    names(FV) = 0:1
+    FV[paste0(n-1)]
+    
+  }
+  
+  #Devolver el ultimo numero
+  
+}
+
+
+
+#### Definir los parametros de entrada para el método ####
+
+# fx = función a evaluar
+# a = limite inferior
+# b = limite superior
+# N = Cantidad de iteraciones -1
+
+### Definir la función a evaluar
+fx = function(x){(x^2) +  (54/x)}
+
+a = 0
+b = 5
+N = 3
+
+
+fibonacci = function(fx,a,b,N){
+  #### Paso 1 ####
+  L = b-a;L # ESTO VA FIJO TAMBIEN
+  k = 2 # ESTO VA FIJO
+  # Llevar el control interno
+  contador = 0
+  #### Paso 2 ####
+  while(k<=N){
+    # Calcular la razon con numeros de fibonacci para la distancia
+    Lke = as.numeric(FIBO(N-k+1)/FIBO(N+1))*L;Lke
+    x1 = a+Lke
+    x2 = b-Lke
+    
+    #### Paso 3, eliminación de regiones ###
+    fx1 = fx(x1)
+    fx2 = fx(x2)
+    
+    # Decidir que hacer si 
+    if(fx1>fx2){
+      a = x1
+    } else{
+      if(fx1<fx2){
+        b = x2
+      }
+    }
+    ## Evaluar si K == N
+    k = k+1
+  }
+  # Devolver la función
+  return(c(a,b))
+}
+
+
+fibonacci(fx,0,5,10)
+
+
+
+
+
