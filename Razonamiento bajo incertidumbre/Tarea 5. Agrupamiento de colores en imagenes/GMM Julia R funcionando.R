2e91324aed729bd63ea9031c63f8b52a47512e21
poface64 <angelgb064@gmail.com>
2024-10-09 19:49:33 -0600

    GMM funcionando

---
+++ Razonamiento bajo incertidumbre/Tarea 5. Agrupamiento de colores en imagenes/GMM Julia R funcionando.R
@@ -0,0 +1,66 @@
+rm(list=ls())
+### Cargar las librerias
+# Cargar las librerias
+# Cargar la imagen en R
+## Cargar librerias necesarias ##
+library(png) # Libreria para leer PNG's
+library(flextable)# LIbreria para hacer tablas bonitas
+library(scatterplot3d) #gráficar en 3D
+library(rsvg) # Manipular archivos SVG
+library(jpeg) # Comprimir el SVG a JPEG
+library(rgl) # OpenGL pero para los gráficos en 3D
+library(corrplot) # Para gráficar las correlaciones
+library(NbClust) # Correr el Kmedias
+library(JuliaCall) # Para pasarme objetos a Julia
+#### Función para leer imagenes y mapearlas ###
+pixeles = function(imagen){
+  # Extraer los datos
+  datos = cbind.data.frame(R = as.vector(imagen[ , ,1]), # Extraer inf de R
+                           G = as.vector(imagen[ , ,2]), # Extraer inf de G
+                           B = as.vector(imagen[ , ,3])) # Extraer inf de B
+  # Extraer las coordenadas de cada pixel para re-mapearlos
+  #Filas
+  nf = dim(imagen)[1]
+  #Columnas
+  nc = dim(imagen)[2]
+  # Hacer el ordenamiento para remapearlos
+  X = rep(nf:1,nc)
+  Y = rep(1:nc,each = nf)
+  datos$X = X
+  datos$Y = Y
+  # Añadir el vector de colores al conjunto de datos
+  datos$color = rgb(datos$R,datos$G,datos$B)
+  # Regresar el objeto transformado
+  return(datos)
+}
+
+### Función para llamar el codigo fuente de Julia ###
+#julia_source("Kmedias Angel Julia.jl")
+julia_source("GMM en julia.jl")
+#### Imagen 1: Denisse Guerrero ####
+### Cargando la imagen desde la ruta local
+ruta1 = "IMAGENES/IM8.jpeg" # Ruta local de la imagen
+imagen1 = readJPEG(ruta1) # Descomponer la imagen en sus canales RGB
+datos1 = pixeles(imagen1)
+### Cargar las cosas de Julia
+re = proc.time()
+prueba1 = julia_call("gausiano",5,datos1[,1:3]) 
+proc.time()-re
+
+datos1$etiqueta = prueba1$Etiqueta
+datos1$ecolor = ""
+#### Hacer el vector de medias
+for(i in 1:length(unique(datos1$etiqueta)) ){
+  ### Calcular la media
+  media = colMeans(datos1[datos1$etiqueta==i ,1:3])
+  ### Calcular y agregar el nuevo color
+  datos1$ecolor[datos1$etiqueta==i] = rgb(media[1],media[2],media[3])
+}
+
+plot3d(x = datos1$Y,
+       y = 1,
+       z = datos1$X,
+       col = datos1$ecolor)
+
+
+
---
+++ Razonamiento bajo incertidumbre/Tarea 5. Agrupamiento de colores en imagenes/GMM en julia.jl
@@ -0,0 +1,30 @@
+
+# Cargar las paqueterias para la mezcla de Gaussianas
+
+# Ajustar el modelo GMM con 3 componentes (ya que sabemos que hay 3 especies en iris)
+using DataFrames
+using GaussianMixtures
+using RDatasets
+using Statistics
+
+
+function gausiano(K,X)
+    ### indicarle que K es un entero
+    K = Int(K)
+    ### Indicarle que X es una Matrix
+    X = Matrix(X)
+    ### Calcular el modelo de mezcla de Gaussiana
+    gmm = GMM(K, X)
+    ### Calcular los posteriores
+    A = (gmmposterior(gmm,X))
+    ### Extraer las etiquetas
+    Z = Matrix(A[1])
+    etiqueta = [argmax(row) for row in eachrow(Z)]
+    return(DataFrame(Etiqueta = etiqueta))    
+end
+
+
+
+
+
+
